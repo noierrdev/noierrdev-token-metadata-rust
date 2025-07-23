3ca5a1875f7403b3ade65ed447647d39c069e95b
vander moleker <vandermoleker@gmail.com>
2025-07-23 03:48:57 -0700

    updte

--- src/main.rs
+++ src/main.rs
@@ -65,9 +65,9 @@ async fn main() {
 fn get_metadata_pda(mint: &Pubkey) -> Pubkey {
     let metadata_seeds = &[
         b"metadata",
-        &Pubkey::from_str(metaplex_program).unwrap().to_bytes(),
+        &Pubkey::from_str_const(metaplex_program).to_bytes(),
         &mint.to_bytes(),
     ];
-    let (pda, _bump) = Pubkey::find_program_address(metadata_seeds, &Pubkey::from_str(metaplex_program).unwrap());
+    let (pda, _bump) = Pubkey::find_program_address(metadata_seeds, &Pubkey::from_str_const(metaplex_program));
     pda
 }
